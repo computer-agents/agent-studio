320bfad2231a516e7b27c70c3d5c070f968f8ce7
thisiszy <43633145+thisiszy@users.noreply.github.com>
2024-01-06 16:39:05 +0100

    remove test code

--- desktop_env/eval/google_evaluators/calendar_evaluator.py
+++ desktop_env/eval/google_evaluators/calendar_evaluator.py
@@ -141,34 +141,4 @@ class GoogleCalendarEvaluator(Evaluator):
             print(f"An error occurred: {e}, score may be incorrect")
             score = 0.0
 
-        # TODO
         return score
-    
-
-if __name__ == "__main__":
-    gcalendar_service = GoogleCalendarService(token_path="token.json")
-    evaluator = GoogleCalendarEvaluator()
-    
-    # Create an event
-    event = gcalendar_service.create_event(
-            summary='Meeting with Team',
-            location='Office',
-            description='Discuss project status',
-            start_time='2024-01-05T09:00:00',
-            end_time='2024-01-05T10:00:00',
-            attendees=['example@email.com']
-        )
-    print(f"Created event: {event.get('htmlLink')}")
-
-    event_info = gcalendar_service.get_event(event_id=event.get('id'))
-    print(json.dumps(event_info, indent=2))
-
-    # Search events
-    events = gcalendar_service.search_events(start_time='2024-01-05T00:00:00Z', end_time='2024-01-05T23:59:59Z')
-    print(json.dumps(events, indent=2))
-
-    assert evaluator("desktop_env/eval/examples/google_calendar.json") == 1.0
-
-    # Delete an event
-    assert gcalendar_service.delete_event(event_id=event.get('id')) == True
-    print("Succ")
