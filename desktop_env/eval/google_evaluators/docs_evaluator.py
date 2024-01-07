50f6872573220e2e7c821e26d3850356899a9068
wyx <wangyuxics@outlook.com>
2024-01-07 22:21:07 +0800

    port update

--- desktop_env/eval/google_evaluators/docs_evaluator.py
+++ desktop_env/eval/google_evaluators/docs_evaluator.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", scopes)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=51152)
         # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())
