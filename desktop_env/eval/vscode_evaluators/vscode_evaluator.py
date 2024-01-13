81700a68375bf04c42e78d5002921bbae0a7ce56
wyx <wangyuxics@outlook.com>
2024-01-13 09:01:15 +0800

    merge main

--- desktop_env/eval/vscode_evaluators/vscode_evaluator.py
+++ desktop_env/eval/vscode_evaluators/vscode_evaluator.py
@@ -3,11 +3,17 @@ import os
 from rich import print
 
 
-def check_vscode_extension(extension_name):
+def installed_vscode_extension(extension_name):
     output = os.popen("code --list-extensions").readlines()
     output = [line.strip() for line in output]  # removes \n
     return extension_name in output
 
+def uninstall_vscode_extension(extension_name):
+    not installed_vscode_extension(extension_name)
+
 
 if __name__ == "__main__":
-    print(check_vscode_extension("ms-python.python"))
+    print('installed_vscode_extension',installed_vscode_extension("ms-python.python"))
+    print('uninstall_vscode_extension',uninstall_vscode_extension("ms-python.python"))
+
+
