aec1844640b23b8b6aaa3a2ad4ea02f212e966d4
thisiszy <43633145+thisiszy@users.noreply.github.com>
2024-02-15 17:45:49 +0100

    Adapt process evaluator to the new framework

--- playground/env/desktop_env/eval/os_evaluators/process_evaluator.py
+++ playground/env/desktop_env/eval/os_evaluators/process_evaluator.py
@@ -10,62 +10,79 @@ from playground.env.desktop_env.eval.evaluator import (
     evaluation_handler,
     reset_handler,
 )
+from playground.utils.human_utils import confirm_action
 
 logger = logging.getLogger(__name__)
 
 
+def find_procs_by_name(name: str) -> list[psutil.Process]:
+    ls = []
+    template = re.compile(name)
+    for p in psutil.process_iter():
+        name_, exe = "", ""
+        try:
+            name_ = p.name()
+            exe = p.exe()
+        except (
+            psutil.AccessDenied,
+            psutil.ZombieProcess,
+            psutil.NoSuchProcess
+        ):
+            continue
+        if template.match(name_) or template.match(os.path.basename(exe)):
+            ls.append(p)
+    return ls
+
 class ProcessEvaluator(Evaluator):
     name: str = "process"
 
-    @staticmethod
-    def find_procs_by_name(name: str) -> list[psutil.Process]:
-        ls = []
-        template = re.compile(name)
-        for p in psutil.process_iter():
-            name_, exe = "", ""
+    @evaluation_handler("match_process")
+    def match_process(self, name: str) -> None:
+        """
+        Check if a process with the given name is running.
+
+        Args:
+            name (str): Name of the process to check.
+
+        Raises:
+            FeedbackException: If the process is not found.
+        """
+        procs = find_procs_by_name(name)
+        if len(procs) == 0:
+            raise FeedbackException(f"Process with name {name} not found.")
+
+    @reset_handler("create_process")
+    def create_process(self, cmd: list[str]) -> None:
+        """
+        Create a process with the given command.
+
+        Args:
+            cmd (list[str]): Command to create the process.
+
+        Raises:
+            FeedbackException: If the process creation fails.
+        """
+        psutil.Popen(cmd)
+
+    @reset_handler("pkill_by_name")
+    def pkill_by_name(self, name: str) -> None:
+        """
+        Kill all processes with the given name.
+
+        Args:
+            name (str): Name pattern of the process to kill.\
+                Can be a regex pattern.
+
+        Raises:
+            FeedbackException: If the process is not found.
+        """
+        @confirm_action
+        def _kill_process(proc: psutil.Process) -> None:
             try:
-                name_ = p.name()
-                exe = p.exe()
-            except (
-                psutil.AccessDenied,
-                psutil.ZombieProcess,
-                psutil.NoSuchProcess
-            ):
-                continue
-            if template.match(name_) or template.match(os.path.basename(exe)):
-                ls.append(p)
-        return ls
+                proc.kill()
+            except psutil.NoSuchProcess:
+                pass
 
-    def execute(self, steps: list[dict[str, dict[str, Any]]]) -> bool:
-        try:
-            for step in steps:
-                for action, params in step.items():
-                    match action:
-                        case "create_process":
-                            cmd: list[str] = params["cmd"]
-                            psutil.Popen(cmd)
-                        case "pkill_by_name":
-                            process_name: str = params["name"]
-                            for proc in self.find_procs_by_name(process_name):
-                                proc.kill()
-                        case _:
-                            raise Exception(f"Action {action} not found")
-        except Exception as e:
-            logger.error(f"Error executing process evaluator: {e}")
-            return False
-        return True
-
-    def __call__(self) -> float:
-        score = 1.0
-        for approach, value in self.reference_answer.items():
-            match approach:
-                case "match_process":
-                    process_name: str = value["name"]
-                    procs = self.find_procs_by_name(process_name)
-                    if len(procs) == 0:
-                        score = 0.0
-                        break
-                case _:
-                    raise Exception(f"Approach {approach} not found")
-
-        return score
+        for proc in find_procs_by_name(name):
+            print("Killing process: ", proc)
+            _kill_process(proc)
--- tests/test_evaluators/test_process.py
+++ tests/test_evaluators/test_process.py
@@ -1,42 +1,38 @@
-import json
+# pytest -s tests/test_evaluators/test_process.py
+import pytest
 
-from playground.agent.teacher_forcing_agent import TeacherForcingAgent
-from playground.desktop_env import ComputerEnv
-from playground.desktop_env.eval.evaluator_helper import evaluator_router
+from playground.env.desktop_env.eval.evaluator_helper import evaluator_router
 
+TASK_CONFIGS = [
+    {
+        "evals": [
+            {
+                "eval_type": "process",
+                "eval_procedure": [
+                    {
+                        "match_process": {
+                            "name": "(?<!\\w)(?i:code)(?!\\w)"
+                        },
+                    }
+                ],
+                "reset_procedure": [
+                    {
+                        "pkill_by_name": {
+                            "name": "(?<!\\w)(?i:code)(?!\\w)"
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+]
 
-def test_vscode(
-    computer_env: ComputerEnv,
-) -> None:
-    config_file = "playground/desktop_env/eval/tasks/process.json"
-    with open(config_file, "r") as f:
-        task_configs = json.load(f)
-    agent = TeacherForcingAgent(env=computer_env)
+@pytest.mark.parametrize("task_config", TASK_CONFIGS)
+def test_process(task_config):
+    comb = evaluator_router(task_config)
+    comb.reset()
+    score, feedback = comb()
+    assert score == 1.0, feedback
 
-    for task_config in task_configs["tasks"]:
-        comb = evaluator_router(task_config)
-        comb.reset()
-
-        action_sequence_path: str | None = task_configs.get(
-            "action_sequence_path", None
-        )
-        if action_sequence_path is not None:
-            with open(action_sequence_path, "r") as f:
-                tasks = json.load(f)["tasks"]
-                for t in tasks:
-                    if t["task_id"] == task_config["task_id"]:
-                        reference_action_sequence = t["reference_action_sequence"]
-                        break
-        else:
-            reference_action_sequence = ""
-        instruction = task_config["intent_template"].format(
-            **task_config["instantiation_dict"]
-        )
-        agent.reset(
-            instruction=instruction,
-            reference_action_sequence=reference_action_sequence,
-        )
-        agent.run()
-
-        score = comb()
-        assert score == 1.0
+if __name__ == "__main__":
+    test_process(TASK_CONFIGS[0])
