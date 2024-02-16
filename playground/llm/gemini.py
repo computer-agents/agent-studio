84bcb79f00d653e2d06c4db74fdaf86c9f492ab3
thisiszy <43633145+thisiszy@users.noreply.github.com>
2024-02-16 16:37:50 +0100

    minor fix

--- playground/llm/gemini.py
+++ playground/llm/gemini.py
@@ -1,14 +1,17 @@
 import logging
 from typing import Any
+import pickle
+import base64
 
 import backoff
 import google.generativeai as genai
 import numpy as np
-
+import requests
 # Magic import, add following import to fix bug
 # https://github.com/google/generative-ai-python/issues/178
 import PIL.PngImagePlugin
 from google.generativeai.types import GenerationConfig
+
 from numpy.typing import NDArray
 from PIL import Image
 
@@ -24,9 +27,10 @@ logger = logging.getLogger(__name__)
 
 
 class GeminiProvider(BaseModel):
-    def __init__(self, **kwargs: Any) -> None:
+    def __init__(self) -> None:
         genai.configure(api_key=config.GEMINI_API_KEY)
-        model = kwargs.get("model", config.model)
+        model = config.model
+        self.proxy = config.model_proxy
         self.model = genai.GenerativeModel(model)
 
     def _compose_messages(
@@ -40,11 +44,17 @@ class GeminiProvider(BaseModel):
             messages.append({"role": "user", "parts": [system_prompt]})
         for step in trajectory:
             img = Image.fromarray(np.uint8(step["obs"])).convert("RGB")
-            user_content = [img]
+            user_content: list[Image.Image|str] = [img]
+            if "act" in step:
+                user_content.append(
+                    f"[Action]: \n{step['act']}"
+                )
             if "res" in step:
-                user_content.append(step["res"])
+                user_content.append(
+                    f"[Result]: \n{step['res']}"
+                )
             if messages[-1]["role"] == "user":
-                messages[-1]["parts"].append(*user_content)
+                messages[-1]["parts"].extend(user_content)
             else:
                 messages.append(
                     {
@@ -52,7 +62,6 @@ class GeminiProvider(BaseModel):
                         "parts": user_content,
                     }
                 )
-            messages.append({"role": "model", "parts": [step["act"]]})
         img = Image.fromarray(np.uint8(obs)).convert("RGB")
         user_content = [img]
         if messages[-1]["role"] == "user":
@@ -79,7 +88,7 @@ class GeminiProvider(BaseModel):
             # top_p=kwargs.get("top_p", config.max_tokens),
             # top_k=kwargs.get("top_k", config.max_tokens),
             candidate_count=1,
-            max_output_tokens=kwargs.get("max_tokens", config.max_tokens),
+            # max_output_tokens=kwargs.get("max_tokens", config.max_tokens),
         )
         logger.info(f"Creating chat completion with model {model}...")
 
@@ -90,10 +99,34 @@ class GeminiProvider(BaseModel):
             interval=10,
         )
         def _generate_response_with_retry() -> tuple[str, dict[str, int]]:
-            response = self.model.generate_content(
-                contents=messages, generation_config=generation_config
-            )
-            message = response.text
+            if self.proxy:
+                body = {
+                    "messages": [
+                        base64.b64encode(
+                            pickle.dumps(messages)
+                        ).decode('utf-8'),
+                        base64.b64encode(
+                            pickle.dumps(generation_config)
+                        ).decode('utf-8')
+                    ]
+                }
+                response = requests.post(
+                    self.proxy,
+                    json=body
+                )
+                try:
+                    message = pickle.loads(base64.b64decode(response.text.encode('utf-8'))).text
+                except Exception as e:
+                    print(e)
+                    print(response.text)
+                    print(pickle.loads(base64.b64decode(response.text.encode('utf-8'))))
+                    print(pickle.loads(base64.b64decode(response.text.encode('utf-8'))).parts)
+                    raise e
+            else:
+                response = self.model.generate_content(
+                    contents=messages, generation_config=generation_config
+                )
+                message = response.text
             info: dict[str, int] = {}
 
             logger.info(f"Response received from {model}")
