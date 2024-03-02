5b5328425119f207840a1c399c563548d02ccd38
thisiszy <43633145+thisiszy@users.noreply.github.com>
2024-03-02 22:03:27 +0100

    fix bugs

--- run.py
+++ run.py
@@ -17,6 +17,15 @@ from playground.env.desktop_env.recorder.screen_recorder import (
     ScreenRecorder,
     VNCRecorder,
 )
+from playground.utils.communication import (
+    PlaygroundResetRequest,
+    PlaygroundResponse,
+    PlaygroundResultResponse,
+    PlaygroundStatusResponse,
+    PlaygroundTextRequest,
+    PlaygroundEvalRequest,
+)
+from playground.agent.base_agent import Agent
 from playground.env.desktop_env.vnc_client import VNCStreamer
 from playground.llm import setup_model
 from playground.utils.human_utils import confirm_action
@@ -77,6 +86,228 @@ def setup_tasks(args):
     return task_configs
 
 
+def wait_finish():
+    while True:
+        response_raw = requests.get(
+            f"http://{config.env_server_addr}:{config.env_server_port}/task/status"
+        )
+        response = PlaygroundStatusResponse(**response_raw.json())
+        if response.status == "finished":
+            break
+        elif response.status == "wait_for_input":
+            if config.need_human_confirmation:
+                user_input = input(f"{response.content}")
+            else:
+                user_input = "y"
+            response_raw = requests.post(
+                url=f"http://{config.env_server_addr}:{config.env_server_port}/task/confirm",  # noqa: E501
+                json=PlaygroundTextRequest(message=user_input).model_dump(),
+            )
+            response = PlaygroundResponse(**response_raw.json())
+            assert response.status == "success"
+        elif response.status in ["pending", "in_progress"]:
+            pass
+        else:
+            raise ValueError(f"Unknown status: {response.status}")
+        time.sleep(1)
+
+
+def eval_gui(
+    agent: Agent,
+    task_configs: list[dict],
+    record_path: str,
+) -> None:
+    """Evaluate the agent on the given tasks with GUI."""
+    try:
+        if not config.remote:
+            import atexit
+
+            local_agent_server = psutil.Popen(
+                [
+                    "python",
+                    "scripts/agent_server.py",
+                    "--env",
+                    "desktop",
+                ]
+            )
+
+            def cleanup(local_agent_server: psutil.Process):
+                local_agent_server.terminate()
+                local_agent_server.wait()
+
+            atexit.register(cleanup, local_agent_server)
+
+        while True:
+            try:
+                response = requests.get(
+                    f"http://{config.env_server_addr}:"
+                    f"{config.env_server_port}/health"
+                )
+                if response.status_code == 200:
+                    break
+            except Exception:
+                logger.info("Waiting for the agent server to start...")
+                time.sleep(1)
+
+        app = QApplication(sys.argv)
+        interface = AgentInterface(
+            agent=agent,
+            task_configs=task_configs,
+            record_path=record_path,
+        )
+        interface.showMaximized()
+        sys.exit(app.exec())
+    except asyncio.exceptions.CancelledError:
+        sys.exit(0)
+
+
+
+def eval_headless(
+    agent: Agent,
+    task_configs: list[dict],
+    record_path: str,
+) -> None:
+    """Evaluate the agent on the given tasks in Terminal."""
+    scores = {}
+    screen_recorder: ScreenRecorder | None = None
+    vnc_streamer: VNCStreamer | None = None
+    for task_config in task_configs:
+        try:
+            task_id = task_config["task_id"]
+            if config.remote:
+                if task_config["visual"]:
+                    vnc_streamer = VNCStreamer(
+                        env_server_addr=config.env_server_addr,
+                        vnc_port=config.vnc_port,
+                        vnc_password=config.vnc_password,
+                    )
+                    vnc_streamer.start()
+                    screen_recorder = VNCRecorder(
+                        fps=config.video_fps,
+                        vnc_streamer=vnc_streamer,
+                    )
+                    screen_recorder.start()
+                else:
+                    screen_recorder = None
+
+                response_raw = requests.post(
+                    f"http://{config.env_server_addr}:{config.env_server_port}/task/reset",
+                    json=PlaygroundResetRequest(task_config=task_config).model_dump(),
+                )
+                response = PlaygroundResponse(**response_raw.json())
+                assert response.status == "submitted"
+                wait_finish()
+                response_raw = requests.get(
+                    f"http://{config.env_server_addr}:{config.env_server_port}/task/result",
+                )
+                response = PlaygroundResultResponse(**response_raw.json())
+                # TODO: handle failed reset
+                assert response.status == "finished" and response.result == "success"
+
+                instruction = task_config["instruction"]
+                logger.info(f"Task instruction: {instruction}")
+            else:
+                if task_config["visual"]:
+                    screen_recorder = ScreenRecorder(
+                        fps=config.video_fps,
+                    )
+                    screen_recorder.start()
+                else:
+                    screen_recorder = None
+                comb = evaluator_router(task_config)
+                comb.reset()
+
+                instruction = task_config["instruction"]
+                logger.info(f"Task instruction: {instruction}")
+
+            agent.reset(instruction=instruction)
+            # Loop until the task is done or the max step is reached.
+            for t in range(config.max_step):
+                logger.info(f"Step {t}")
+                if task_config["visual"]:
+                    assert screen_recorder is not None
+                    obs = screen_recorder.get_current_frame()
+                else:
+                    obs = None
+                agent.generate_action(obs)
+                if config.need_human_confirmation:
+                    confirmed, _ = confirm_action()(lambda: True)()
+                else:
+                    confirmed = True
+                _, done = agent.step_action(confirmed)
+                time.sleep(config.minimal_action_interval)
+                if done:
+                    break
+
+            task_trajectory_path = (
+                Path(record_path) / task_config["task_id"]
+            )
+            task_trajectory_path.mkdir(parents=True, exist_ok=True)
+            if task_config["visual"]:
+                assert screen_recorder is not None
+                screen_recorder.stop()
+                screen_recorder.wait_exit()
+                video_path = (task_trajectory_path / "video.mp4").as_posix()
+                screen_recorder.save(video_path, 0)
+                screen_recorder = None
+                logger.info(f"Video saved to {video_path}")
+            else:
+                video_path = None
+
+            if config.remote:
+                response_raw = requests.post(
+                    f"http://{config.env_server_addr}:{config.env_server_port}/task/eval",
+                    json=PlaygroundEvalRequest(
+                        task_config=task_config,
+                    ).model_dump(),
+                )
+                response = PlaygroundResponse(**response_raw.json())
+                assert response.status == "submitted"
+                wait_finish()
+                response_raw = requests.get(
+                    f"http://{config.env_server_addr}:{config.env_server_port}/task/result"
+                )
+                response = PlaygroundResultResponse(**response_raw.json())
+                assert response.status == "finished" and isinstance(response.message, dict)
+                score, feedback = response.message["score"], response.message["feedback"]
+            else:
+                logger.info("Start evaluation")
+                score, feedback = comb()
+
+            scores[task_id] = score
+            if score == 1.0:
+                logger.info(f"[Result] (PASS): {feedback}")
+            else:
+                logger.info(f"[Result] (FAIL): {feedback}")
+            export_trajectories(
+                agent=agent,
+                task_config=task_config,
+                trajectory=agent.trajectory,
+                record_path=record_path,
+                score=score,
+                feedback=feedback,
+            )
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            import traceback
+
+            logger.error(f"[Unhandled Error] {repr(e)}]")
+            logger.error(traceback.format_exc())
+        finally:
+            if screen_recorder is not None:
+                screen_recorder.stop()
+                screen_recorder.wait_exit()
+                screen_recorder = None
+            if vnc_streamer is not None:
+                vnc_streamer.stop()
+                vnc_streamer = None
+    agent.close()
+    logger.info(
+        f"Average score: {sum(scores.values()) / max(len(scores), 1)}"
+    )
+
+
 def eval(args) -> None:
     """Evaluate the agent on the given tasks."""
 
@@ -86,162 +317,9 @@ def eval(args) -> None:
     match args.env:
         case "desktop":
             if not config.headless:
-                # Run evaluation with GUI.
-                # from playground.env.desktop_env.agent_interface import run_ui
-
-                try:
-                    if not config.remote:
-                        import atexit
-
-                        local_agent_server = psutil.Popen(
-                            [
-                                "python",
-                                "scripts/agent_server.py",
-                                "--env",
-                                "desktop",
-                            ]
-                        )
-
-                        def cleanup(local_agent_server: psutil.Process):
-                            local_agent_server.terminate()
-                            local_agent_server.wait()
-
-                        atexit.register(cleanup, local_agent_server)
-
-                    while True:
-                        try:
-                            response = requests.get(
-                                f"http://{config.env_server_addr}:"
-                                f"{config.env_server_port}/health"
-                            )
-                            if response.status_code == 200:
-                                break
-                        except Exception:
-                            logger.info("Waiting for the agent server to start...")
-                            time.sleep(1)
-
-                    app = QApplication(sys.argv)
-                    interface = AgentInterface(
-                        agent=agent,
-                        task_configs=task_configs,
-                        record_path=record_path,
-                    )
-                    interface.showMaximized()
-                    sys.exit(app.exec())
-                    # else:
-                    #     qasync.run(
-                    #         run_ui(
-                    #             agent=agent,
-                    #             task_configs=task_configs,
-                    #             record_path=record_path,
-                    #         )
-                    #     )
-                    # TODO: can we use qasync.run when remote = False?
-                except asyncio.exceptions.CancelledError:
-                    sys.exit(0)
-
+                eval_gui(agent, task_configs, record_path)
             else:
-                # Run evaluation locally, only for text-only tasks.
-                if config.remote:
-                    vnc_streamer = VNCStreamer(
-                        env_server_addr=config.env_server_addr,
-                        vnc_port=config.vnc_port,
-                        vnc_password=config.vnc_password,
-                    )
-                    vnc_streamer.start()
-                scores = {}
-                screen_recorder: ScreenRecorder | None = None
-                for task_config in task_configs:
-                    try:
-                        if task_config["visual"]:
-                            if config.remote:
-                                screen_recorder = VNCRecorder(
-                                    fps=config.video_fps,
-                                    vnc_streamer=vnc_streamer,
-                                )
-                            else:
-                                screen_recorder = ScreenRecorder(
-                                    fps=config.video_fps,
-                                )
-                            screen_recorder.start()
-                        else:
-                            screen_recorder = None
-                        task_id = task_config["task_id"]
-                        comb = evaluator_router(task_config)
-                        comb.reset()
-
-                        instruction = task_config["instruction"]
-                        logger.info(f"Task instruction: {instruction}")
-
-                        agent.reset(instruction=instruction)
-                        # Loop until the task is done or the max step is reached.
-                        for t in range(config.max_step):
-                            logger.info(f"Step {t}")
-                            if task_config["visual"]:
-                                assert screen_recorder is not None
-                                obs = screen_recorder.get_current_frame()
-                            else:
-                                obs = None
-                            agent.generate_action(obs)
-                            if config.need_human_confirmation:
-                                confirmed, _ = confirm_action()(lambda: True)()
-                            else:
-                                confirmed = True
-                            _, done = agent.step_action(confirmed)
-                            time.sleep(config.minimal_action_interval)
-                            if done:
-                                break
-
-                        task_trajectory_path = (
-                            Path(record_path) / task_config["task_id"]
-                        )
-                        task_trajectory_path.mkdir(parents=True, exist_ok=True)
-                        if task_config["visual"]:
-                            assert screen_recorder is not None
-                            screen_recorder.stop()
-                            screen_recorder.wait_exit()
-                            video_path = (task_trajectory_path / "video.mp4").as_posix()
-                            screen_recorder.save(video_path, 0)
-                            screen_recorder = None
-                            logger.info(f"Video saved to {video_path}")
-                        else:
-                            video_path = None
-
-                        logger.info("Start evaluation")
-                        score, feedback = comb()
-                        scores[task_id] = score
-                        if score == 1.0:
-                            logger.info(f"[Result] (PASS): {feedback}")
-                        else:
-                            logger.info(f"[Result] (FAIL): {feedback}")
-
-                        export_trajectories(
-                            agent=agent,
-                            task_config=task_config,
-                            trajectory=agent.trajectory,
-                            record_path=record_path,
-                            score=score,
-                            feedback=feedback,
-                        )
-                    except KeyboardInterrupt:
-                        agent.close()
-                        if config.remote:
-                            vnc_streamer.stop()
-                        if screen_recorder is not None:
-                            screen_recorder.stop()
-                            screen_recorder.wait_exit()
-                        sys.exit(0)
-                    except Exception as e:
-                        import traceback
-
-                        logger.error(f"[Unhandled Error] {repr(e)}]")
-                        logger.error(traceback.format_exc())
-
-                agent.close()
-                logger.info(
-                    f"Average score: {sum(scores.values()) / max(len(scores), 1)}"
-                )
-
+                eval_headless(agent, task_configs, record_path)
         case _:
             raise ValueError(f"Invalid env: {args.env}.")
 
