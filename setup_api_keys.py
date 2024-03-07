4b862d017254e45470188b9fb905f01e94739192
thisiszy <43633145+thisiszy@users.noreply.github.com>
2024-03-07 14:44:44 +0100

    format files

--- setup_api_keys.py
+++ setup_api_keys.py
@@ -1,10 +1,24 @@
-from playground.env.desktop_env.eval.google_evaluators.calendar_evaluator import GoogleCalendarEvaluator
-from playground.env.desktop_env.eval.google_evaluators.drive_evaluator import GoogleDriveEvaluator
-from playground.env.desktop_env.eval.google_evaluators.gmail_evaluator import GmailEvaluator
-from playground.env.desktop_env.eval.google_evaluators.docs_evaluator import GoogleDocsEvaluator
-from playground.env.desktop_env.eval.google_evaluators.sheets_evaluator import GoogleSheetsEvaluator
-from playground.env.desktop_env.eval.google_evaluators.slides_evaluator import GoogleSlidesEvaluator
-from playground.env.desktop_env.eval.google_evaluators.forms_evaluator import GoogleFormsEvaluator
+from playground.env.desktop_env.eval.google_evaluators.calendar_evaluator import (
+    GoogleCalendarEvaluator,
+)
+from playground.env.desktop_env.eval.google_evaluators.docs_evaluator import (
+    GoogleDocsEvaluator,
+)
+from playground.env.desktop_env.eval.google_evaluators.drive_evaluator import (
+    GoogleDriveEvaluator,
+)
+from playground.env.desktop_env.eval.google_evaluators.forms_evaluator import (
+    GoogleFormsEvaluator,
+)
+from playground.env.desktop_env.eval.google_evaluators.gmail_evaluator import (
+    GmailEvaluator,
+)
+from playground.env.desktop_env.eval.google_evaluators.sheets_evaluator import (
+    GoogleSheetsEvaluator,
+)
+from playground.env.desktop_env.eval.google_evaluators.slides_evaluator import (
+    GoogleSlidesEvaluator,
+)
 
 GoogleFormsEvaluator([], [])
 GoogleSlidesEvaluator([], [])
