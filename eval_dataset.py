4095f2eebb22e5bdb42e653c7215df66b8439b25
thisiszy <43633145+thisiszy@users.noreply.github.com>
2024-03-18 23:03:17 +0100

    update eval_dataset and system_prompt

--- eval_dataset.py
+++ eval_dataset.py
@@ -2,6 +2,8 @@
 import argparse
 import logging
 from pathlib import Path
+import ast
+import re
 
 import numpy as np
 from PIL import Image
@@ -31,7 +33,7 @@ def construct_prompt(instruction: str, screenshot: np.ndarray) -> list:
     messages.append(
         {
             "role": "user",
-            "content": f"""You can complete any instruction by writing Python code. You can use the mouse to interact with the environment. `mouse.click(x, y)` can be used to click the mouse at the specified position. You should only return `mouse.click(x, y)` as the action.
+            "content": f"""You can complete any instruction by writing Python code. You can use the mouse to interact with the environment. `mouse.click(x: int, y: int, click_type: str, is_double_click: bool)` can be used to interact with the mouse at the specified position. You can choose "left_click", "right_click" or "middle_click" as the click_type. "is_double_click" indicates whether the click is a double click. You should only return `mouse.click(x, y, click_type, is_double_click)` as the action.
 The task instruction: {instruction}""",  # noqa: E501
         }
     )
@@ -41,9 +43,41 @@ The task instruction: {instruction}""",  # noqa: E501
     return messages
 
 
-def extract_action(response: str) -> tuple[str, int, int]:
+def extract_action(response: str) -> tuple[dict, int, int] | None:
     # TODO: extract action from response by regex (but need to consider four click types)  # noqa: E501
-    return "left", 0, 0
+    logger.info(f"Extracted action: from response: {response}")
+    pattern = r'(mouse\.click\([^)]+\))'
+    matches = re.findall(pattern, response)
+    logger.info(f"Extracted action: {matches}")
+    if len(matches) == 0:
+        return None
+    else:
+        click_type_dict = {
+            "left_click": False,
+            "right_click": False,
+            "middle_click": False,
+            "double_click": False
+        }
+        try:
+            def get_args(expr):
+                tree = ast.parse(expr)
+                if not isinstance(tree.body[0], ast.Expr):
+                    raise ValueError("Invalid action")
+                if not isinstance(tree.body[0].value, ast.Call):
+                    raise ValueError("Invalid action")
+                args = tree.body[0].value.args
+                return [arg.value for arg in args]
+            x, y, click_type, is_double_click = get_args(matches[0])
+            if not (isinstance(x, int) and isinstance(y, int) and \
+                isinstance(click_type, str) and isinstance(is_double_click, bool)):
+                raise ValueError("Invalid action")
+            click_type_dict[click_type] = True
+            if is_double_click:
+                click_type_dict["double_click"] = True
+        except (ValueError, IndexError) as e:
+            logger.warn(f"Failed to extract action: {e}")
+            return None
+    return click_type_dict, x, y
 
 
 def main():
@@ -69,10 +103,7 @@ def main():
 
         annotation = trajectory[0]["annotation"]
         mouse_action = annotation["mouse_action"]
-        click_type = None
-        for k, v in mouse_action["click_type"].items():
-            if v:
-                click_type = k
+        click_type = mouse_action["click_type"]
         assert click_type is not None
         left_x = mouse_action["x"]
         right_x = mouse_action["x"] + mouse_action["width"]
@@ -80,6 +111,7 @@ def main():
         bottom_y = mouse_action["y"] + mouse_action["height"]
 
         # query LLMs
+        score = 0.0
         conversations = []
         assert args.max_retry == 1, "We don't have reflection prompt yet."
         for t in range(args.max_retry):
@@ -92,7 +124,11 @@ def main():
                     "total_tokens": info["total_tokens"],
                 }
             )
-            predicted_click_type, predicted_x, predicted_y = extract_action(response)
+            predicted_action = extract_action(response)
+            if predicted_action is None:
+                continue
+            else:
+                predicted_click_type, predicted_x, predicted_y = predicted_action
 
             click_type_match = predicted_click_type == click_type
             location_match = (
@@ -116,12 +152,12 @@ def main():
         save_path = Path(args.save_path)
         save_path.mkdir(parents=True, exist_ok=True)
         add_jsonl(
-            {
+            [{
                 "conversations": conversations,
                 "score": score,
                 "click_type_match": click_type_match,
                 "location_match": location_match,
-            },
+            }],
             args.save_path,
         )
 
