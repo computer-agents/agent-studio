7bab791f918f83b27ce142d5750f2cac230f0f6b
Longtao Zheng <longtao001@e.ntu.edu.sg>
2024-06-10 18:02:04 +0800

    update recaptioning prompt for cogvlm

--- evals/re_caption_gui_grounding_data.py
+++ evals/re_caption_gui_grounding_data.py
@@ -16,8 +16,15 @@ Caption the content within the red bounding box with an 'Click on' instruction. 
 """.strip()  # noqa: E501
 
 COGVLM2_PROMPT = """
-Caption the content within the red bounding box with an instruction starting with 'Click on'. The instruction should contain only description of the icon or elements. Make sure your description can be uniquely mapped into the content.
-Instruction:
+Refine the original instruction for the content within the red bounding box with a short instruction starting with 'Click on' with no more than 10 words. If the original instruction is not clear or NA, you can provide instruction based on the image.
+Examples:
+Original instruction: redirect_discounts
+Cleaned instruction: Click on "Discounts".
+Original instruction: view_IMAX_at_AMC
+Cleaned instruction: Click on 'IMAX at AMC'.
+
+Original instruction: {instruction}
+Cleaned instruction:
 """.strip()  # noqa: E501
 
 
@@ -77,11 +84,6 @@ def main():
 
     model = setup_model(args.provider)
     data = read_jsonl(args.data_path, args.start_idx, args.end_idx)
-    if args.provider == "huggingface":
-        prompt_template = COGVLM2_PROMPT
-        kwargs = {"max_new_tokens": 64}
-    else:
-        prompt_template = GPT4O_PROMPT
 
     image_dir = Path(args.data_path).parent / "images"
     save_path = args.data_path.replace("_raw", "")
@@ -91,8 +93,13 @@ def main():
         image_path = os.path.join(image_dir, row["image"])
         messages = [
             {"role": "user", "content": draw_bbox(image_path, row["bbox"])},
-            {"role": "user", "content": prompt_template},
         ]
+        if args.provider == "huggingface":
+            kwargs = {"max_new_tokens": 64}
+            messages.append({"role": "user", "content": COGVLM2_PROMPT.format(instruction=row["instruction"])})
+        else:
+            messages.append({"role": "user", "content": GPT4O_PROMPT})
+
         response, info = model.generate_response(messages, model=args.model, **kwargs)
         row["raw_instruction"] = row["instruction"]
         row["instruction"] = response
