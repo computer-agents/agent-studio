b9a09e6e1df08eff1b35d9885d4bef522b919996
thisiszy <43633145+thisiszy@users.noreply.github.com>
2024-08-18 04:04:22 +0200

    format

--- scripts/recorded_trajectory2episode.py
+++ scripts/recorded_trajectory2episode.py
@@ -1,8 +1,9 @@
-import os
-from pathlib import Path
-from agent_studio.utils.types import Episode
-import shutil
 import argparse
+import os
+import shutil
+from pathlib import Path
+
+from agent_studio.utils.types import Episode
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", type=str)
@@ -34,13 +35,13 @@ except ValueError:
 jsonl_path = jsonl_folder / "output.jsonl"
 
 for dir in os.listdir(input_folder):
-    if dir == '.DS_Store':
+    if dir == ".DS_Store":
         continue
-    if not os.path.exists(f'{input_folder}/{dir}/trajectory.jsonl'):
+    if not os.path.exists(f"{input_folder}/{dir}/trajectory.jsonl"):
         print("skip", dir)
         continue
     print(dir)
-    with open(f'{input_folder}/{dir}/trajectory.jsonl', 'r') as f:
+    with open(f"{input_folder}/{dir}/trajectory.jsonl", "r") as f:
         for line in f:
             epi = Episode.model_validate_json(line)
             image_folder_epi = image_folder / epi.annotation_id
@@ -49,17 +50,25 @@ for dir in os.listdir(input_folder):
             image_folder_epi.mkdir(parents=True, exist_ok=True)
 
             for action in epi.actions:
-                # copy image to image folder and change the path for obs_before and obs_after
+                # copy image to image folder
+                # and change the path for obs_before and obs_after
                 if action.obs_before:
                     src = f"{input_folder}/{dir}/{action.obs_before}"
                     shutil.copy(src, image_folder_epi)
-                    action.obs_before = (image_folder_epi/Path(src).name).relative_to(jsonl_folder).as_posix()
+                    action.obs_before = (
+                        (image_folder_epi / Path(src).name)
+                        .relative_to(jsonl_folder)
+                        .as_posix()
+                    )
                 if action.obs_after:
                     src = f"{input_folder}/{dir}/{action.obs_after}"
                     shutil.copy(src, image_folder_epi)
-                    action.obs_after = (image_folder_epi/Path(src).name).relative_to(jsonl_folder).as_posix()
+                    action.obs_after = (
+                        (image_folder_epi / Path(src).name)
+                        .relative_to(jsonl_folder)
+                        .as_posix()
+                    )
 
             # write a line to output.jsonl, write line by line
-            with open(jsonl_path, 'a') as out_f:
-                out_f.write(epi.model_dump_json() + '\n')
-
+            with open(jsonl_path, "a") as out_f:
+                out_f.write(epi.model_dump_json() + "\n")
