2054d875a3f7b28e4878670bf302bc8564200c9d
thisiszy <43633145+thisiszy@users.noreply.github.com>
2024-09-16 23:49:25 +0200

    use ubuntu for vnc

--- scripts/docker_startup.sh
+++ scripts/docker_startup.sh
@@ -3,7 +3,7 @@
 if [ -n "$VNC_PASSWORD" ]; then
     echo -n "$VNC_PASSWORD" > /.password1
     x11vnc -storepasswd "$(cat /.password1)" /.password2
-    chmod 400 /.password*
+    chmod 644 /.password*  # Change permissions to be readable
     sed -i 's/^command=x11vnc.*/& -rfbauth \/.password2/' /etc/supervisor/conf.d/supervisord.conf
     export VNC_PASSWORD=
 fi
@@ -20,22 +20,22 @@ if [ -n "$RESOLUTION" ]; then
     sed -i "s/1024x768/$RESOLUTION/" /usr/local/bin/xvfb.sh
 fi
 
-USER=${USER:-root}
-HOME=/root
-if [ "$USER" != "root" ]; then
-    echo "* enable custom user: $USER"
-    useradd --create-home --shell /bin/bash --user-group --groups adm,sudo "$USER"
-    if [ -z "$PASSWORD" ]; then
-        echo "  set default password to \"ubuntu\""
-        PASSWORD=ubuntu
-    fi
-    HOME=/home/$USER
-    echo "$USER:$PASSWORD" | chpasswd
-    cp -r /root/{.config,.gtkrc-2.0,.asoundrc} "${HOME}"
-    chown -R "$USER":"$USER" "${HOME}"
-    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+USER=ubuntu
+HOME=/home/$USER
+echo "* Setting up for user: $USER"
+if [ -z "$PASSWORD" ]; then
+    echo "  set default password to \"ubuntu\""
+    PASSWORD=ubuntu
 fi
+echo "$USER:$PASSWORD" | chpasswd
+cp -r /root/{.config,.gtkrc-2.0,.asoundrc} "${HOME}"
+chown -R "$USER":"$USER" "${HOME}"
+[ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+
 sed -i -e "s|%USER%|$USER|" -e "s|%HOME%|$HOME|" /etc/supervisor/conf.d/supervisord.conf
+
+# Modify VNC server to run as ubuntu user
+sed -i 's/^command=x11vnc.*/command=su - ubuntu -c "x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :1 -rfbauth \/.password2 -shared -forever"/' /etc/supervisor/conf.d/supervisord.conf
 
 # home folder
 if [ ! -x "$HOME/.config/pcmanfm/LXDE/" ]; then
